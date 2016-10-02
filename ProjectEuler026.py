76d0e2dba3136312f6e775b690faab71786007aa
Edima Udo <edimaudo@gmail.com>
2016-10-02 19:08:29 -0400

    Solution

--- ProjectEuler026.py
+++ ProjectEuler026.py
@@ -1,7 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 26 13:21:57 2016
+def reciprocal_cycle(value):
+    output_list = {}
+    x,i = 1,0
+    while x not in output_list:
+        output_list[x] = i
+        x = x * 10 % value
+        i+=1
+    return i - output_list[x]
+    
 
-@author: edima
-"""
+def main():
+    output = max(range(1,1000),key=reciprocal_cycle)
+    print(str(output))
 
+
+if __name__ == "__main__":
+          main()
